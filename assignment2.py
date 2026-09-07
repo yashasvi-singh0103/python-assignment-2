6c41f3edb21090a31954a0616812e564cbd6966c
yashasvi-singh0103 <yashasvisingh0103@gmail.com>
2026-09-07 07:05:02 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,24 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2870
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 marks = int(input("Enter marks obtained in Mathematics: "))\
+\
+if marks >= 90:\
+    print("Grade: O")\
+elif marks >= 80:\
+    print("Grade: A+")\
+elif marks >= 70:\
+    print("Grade: A")\
+elif marks >= 60:\
+    print("Grade: B")\
+elif marks >= 50:\
+    print("Grade: C")\
+elif marks >= 40:\
+    print("Grade: P")\
+else:\
+    print("Grade: F")\
+}
